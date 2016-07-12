f70470a17073473bf245837a7bfa095501d41f9d
Collin Fingar <collinfingar@gmail.com>
2016-07-12 18:57:56 -0400

    Can now rotate with right analog stick and fire guns attached to camera with the triggers.

--- Research Project/Assets/Scripts/VRPlayer/Movement.cs
+++ Research Project/Assets/Scripts/VRPlayer/Movement.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-    //Movment
+    //MOVEMENT
     public float moveSpeed = 1f;
     public float sprintSpeed = 2f;
 
-    //Rotation
+    //ROTATION
+    //How do you rotate
+    public bool ButtonRotation = true;
+    public bool StickRotation = true;
+    //Rotate speed control
     public float rotationSnap = 90f;
     private bool rotated = false;
+    public float stickRotationSnapInterval = .25f;
+    private float stickLastSnappedTime = 0f;
     
-
 	void Start () {
 	
 	}
@@ -21,17 +26,32 @@ public class Movement : MonoBehaviour {
         HandleMovement();
 	}
 
+    //Gets Gamepad input (Button/Stick) and rotates accordingly
     void HandleRotation() {
-        float value = Input.GetAxis("DHorz");
-        if (value != 0 && !rotated){
-            transform.Rotate(new Vector3(0, value * rotationSnap, 0));
-            rotated = true;
+        if (ButtonRotation) {
+            float value = Input.GetAxis("DHorz");
+            if (value != 0 && !rotated){
+                transform.Rotate(new Vector3(0, value * rotationSnap, 0));
+                rotated = true;
+            }
+            else if (value == 0 && rotated){
+                rotated = false;
+            }
         }
-        else if (value == 0 && rotated) {
-            rotated = false;
+        if (StickRotation) {
+            float stickValue = Input.GetAxis("RightStickX");
+            if (Mathf.Abs(stickValue) > .3f && Time.time > stickLastSnappedTime){
+                int val = 1;
+                if (stickValue < 0){
+                    val = -1;
+                }
+                transform.Rotate(new Vector3(0, val * rotationSnap, 0));
+                stickLastSnappedTime = Time.time + stickRotationSnapInterval;
+            }
         }
     }
 
+    //Gets Gamepad input (Stick) and moves accordingly
     void HandleMovement() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
---
+++ Research Project/Assets/Scripts/VRPlayer/Shoot.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject shotPrefab;
+    public Vector3 shotSpawnOffset = new Vector3(0, 0, .5f);
+    public float frequency = .1f;
+    public float delay = .5f;
+    public bool fullAuto = true;
+    public Vector3 shotVelocity = new Vector3(0, 0, 3);
+    private Vector3 shootDirectionVelocity;
+
+    private float nextShootTime = 0f;
+    private bool shootingAuto = false;
+    private bool semiFired = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SenseShoot();
+	}
+
+    void SenseShoot() {
+        float lTrig = Input.GetAxis("LeftTrigger");
+        float rTrig = Input.GetAxis("RightTrigger");
+        if (!fullAuto) {
+            if (lTrig > .3 && rTrig > .3) {
+                Fire();
+                semiFired = true;
+            } else {
+                semiFired = false;
+            }
+        } else {
+            if (shootingAuto) {
+                if (lTrig > .3 && rTrig > .3 && Time.time > nextShootTime){
+                    Fire();
+                    nextShootTime = Time.time + frequency;
+                } else if(Time.time > nextShootTime){
+                    shootingAuto = false;
+                }
+            } else {
+                if (lTrig > .3 && rTrig > .3){
+                    nextShootTime = Time.time + delay;
+                    shootingAuto = true;
+                }
+            }
+        }
+        
+        
+    }
+
+    void Fire() {
+        shootDirectionVelocity = transform.TransformDirection(shotVelocity);
+        GameObject instance = (GameObject)Instantiate(shotPrefab, transform.position + transform.TransformDirection(shotSpawnOffset), transform.rotation);
+        instance.GetComponent<Rigidbody>().AddForce(shootDirectionVelocity);
+    }
+}
