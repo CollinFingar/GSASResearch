d372b9b90822de64cc6e140139baf8670188f646
Collin Fingar <collinfingar@gmail.com>
2016-07-11 21:05:05 -0400

    We now have a basic moving and rotating script for the VR dude.

--- Research Project/Assets/Scripts/VRPlayer/Movement.cs
+++ Research Project/Assets/Scripts/VRPlayer/Movement.cs
@@ -3,21 +3,51 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    //Movment
     public float moveSpeed = 1f;
-    public float rotationSnap = 90f;
+    public float sprintSpeed = 2f;
 
-	// Use this for initialization
+    //Rotation
+    public float rotationSnap = 90f;
+    private bool rotated = false;
+    
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("A")) {
-            
-        }
-        if (Input.GetAxis("RightTrigger") != 0) {
-            Debug.Log(Input.GetAxis("RightTrigger"));
-        }
+        HandleRotation();
+        HandleMovement();
 	}
+
+    void HandleRotation() {
+        float value = Input.GetAxis("DHorz");
+        if (value != 0 && !rotated){
+            transform.Rotate(new Vector3(0, value * rotationSnap, 0));
+            rotated = true;
+        }
+        else if (value == 0 && rotated) {
+            rotated = false;
+        }
+    }
+
+    void HandleMovement() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        bool stick = Input.GetButton("LeftStickButton");
+
+        if (x != 0 || z != 0) {
+            Vector3 moveVec = new Vector3(x, 0, z);
+            float speed = 0;
+            if (stick && z > 0) {
+                speed = sprintSpeed;
+            } else {
+                speed = moveSpeed;
+            }
+
+            moveVec = transform.TransformDirection(moveVec) * speed * Time.deltaTime * 2;
+            transform.position += moveVec;
+        }
+    }
 }
