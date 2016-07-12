cf8b18e718454e554a529443a4f660525daf4806
Collin Fingar <collinfingar@gmail.com>
2016-07-11 20:21:44 -0400

    Started work on VR movement. Mapped XBOX controller input to the input manager.

---
+++ Research Project/Assets/Scripts/VRPlayer/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 1f;
+    public float rotationSnap = 90f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("A")) {
+            
+        }
+        if (Input.GetAxis("RightTrigger") != 0) {
+            Debug.Log(Input.GetAxis("RightTrigger"));
+        }
+	}
+}
