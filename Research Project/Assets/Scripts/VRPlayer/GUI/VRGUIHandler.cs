95df77c0d4e32392e747a5b1f1451ecbfc53937d
Collin Fingar <collinfingar@gmail.com>
2016-10-06 23:02:12 -0400

    Guns light up when charged. You might ask "Wait Collin... won't you die from the sheer awesomeness?" Well... That's a risk I'm going to take.

--- Research Project/Assets/Scripts/VRPlayer/GUI/VRGUIHandler.cs
+++ Research Project/Assets/Scripts/VRPlayer/GUI/VRGUIHandler.cs
@@ -1,15 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VRGUIHandler : MonoBehaviour {
 
+	public int energyAmountTotal = 15;
+	public GameObject energyObject;
+	private GameObject[] energyArray;
+	
+	private int currentEnergyAmount;
+
+
+	public Color highEnergyColor = new Color(1,0,0,.7f);
+	public Color mediumEnergyColor = new Color(1,0,0,.7f);
+	public Color lowEnergyColor = new Color(1,0,0,.7f);
+
+	private int colorStateTracker = 2;
+	//2 = high
+	//1 = medium
+	//0 = low
+
+	public int highColorMinimum = 9;
+	public int mediumColorMinimum = 5;
+
 	// Use this for initialization
 	void Start () {
-	
+		InitializeEnergyArray ();
+		UpdateEnergyColor (colorStateTracker);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+		
+	void InitializeEnergyArray(){
+		currentEnergyAmount = energyAmountTotal;
+		energyArray = new GameObject[energyAmountTotal];
+		int e = 0;
+		foreach (Transform bar in energyObject.transform) {
+			energyArray [e] = bar.gameObject;
+			e++;
+		}
+	}
+
+	void UpdateEnergyLevel(int newAmount){
+		if (newAmount > energyAmountTotal || newAmount < 0 || newAmount == currentEnergyAmount) {
+			return;
+		}
+		CheckEnergyColor (newAmount);
+		currentEnergyAmount = newAmount;
+		for (int e = 0; e < energyAmountTotal; e++) {
+			if (e < currentEnergyAmount) {
+				energyArray [e].SetActive (true);
+			} else {
+				energyArray [e].SetActive (false);
+			}
+		}
+	}
+
+	void CheckEnergyColor(int newAmount){
+		int newColorState = 2;
+		if (newAmount < mediumColorMinimum) {
+			newColorState = 0;
+		} else if (newAmount < highColorMinimum) {
+			newColorState = 1;
+		}
+		if (newColorState != colorStateTracker) {
+			colorStateTracker = newColorState;
+			UpdateEnergyColor (colorStateTracker);
+		}
+	}
+
+	void UpdateEnergyColor(int colorState){
+		Color newColor;
+		if (colorState == 2) {
+			newColor = highEnergyColor;
+		} else if (colorState == 1) {
+			newColor = mediumEnergyColor;
+		} else {
+			newColor = lowEnergyColor;
+		}
+		for (int e = 0; e < energyAmountTotal; e++) {
+			energyArray [e].GetComponent<Image> ().color = newColor;
+		}
 	}
 }
--- Research Project/Assets/Scripts/VRPlayer/Shoot.cs
+++ Research Project/Assets/Scripts/VRPlayer/Shoot.cs
@@ -16,14 +16,16 @@ public class Shoot : MonoBehaviour {
     private bool semiFired = false;
     private bool fireable = false;
 
-    private float minGunScale = .3f;
-    private float maxGunScale = .6f;
-    private float curGunScale = .3f;
-    private float scaleSpeed = 1f;
+	public GameObject[] gunEnergyObjectArray;
+	private SpriteRenderer[] gunEnergySpriteArray;
+	public Color gunEnergyColor = new Color(1,1,1,1);
+	private float alpha = 0f;
+	private float alphaRate = 3f;
+
 
     // Use this for initialization
     void Start () {
-
+		InitializeGunEnergy ();
 	}
 	
 	// Update is called once per frame
@@ -61,11 +63,13 @@ public class Shoot : MonoBehaviour {
 	void SenseAim() {
 		float lTrig = Input.GetAxis("LeftTrigger");
 		if (lTrig > .2) {
-				fireable = true;
+			fireable = true;
+			ChangeColor (true);
 		} else if(lTrig < .2){
 			if (fireable) {
 				fireable = false;
 			}
+			ChangeColor (false);
 		}
 		if (fireable) {
 			SenseShoot();
@@ -77,38 +81,41 @@ public class Shoot : MonoBehaviour {
         GameObject instance = (GameObject)Instantiate(shotPrefab, transform.position + transform.TransformDirection(shotSpawnOffset), transform.rotation);
         instance.GetComponent<Rigidbody>().AddForce(shootDirectionVelocity);
     }
+
+	void InitializeGunEnergy(){
+		gunEnergySpriteArray = new SpriteRenderer[gunEnergyObjectArray.Length];
+		for (int i = 0; i < gunEnergyObjectArray.Length; i++) {
+			SpriteRenderer gunEnergySprite = gunEnergyObjectArray [i].GetComponent<SpriteRenderer> ();
+			gunEnergySpriteArray [i] = gunEnergySprite;
+			gunEnergySprite.color = new Color (gunEnergyColor.r, gunEnergyColor.g, gunEnergyColor.b, 0f);
+		}
+	}
+
+	void ChangeColor (bool aiming){
+		if (aiming && alpha < 1) {
+			float changeAmount = alphaRate * Time.deltaTime;
+			if (alpha + changeAmount >= 1) {
+				alpha = 1;
+			} else {
+				alpha += changeAmount;
+			}
+			UpdateColors ();
+		} else if (!aiming && alpha > 0) {
+			float changeAmount = alphaRate * Time.deltaTime;
+			if (alpha - changeAmount <= 0) {
+				alpha = 0;
+			} else {
+				alpha -= changeAmount;
+			}
+			UpdateColors ();
+		}
+	}
+
+	void UpdateColors(){
+		for (int i = 0; i < gunEnergySpriteArray.Length; i++) {
+			gunEnergySpriteArray[i].color = new Color (gunEnergyColor.r, gunEnergyColor.g, gunEnergyColor.b, alpha);
+		}
+	}
 }
 
 
-
-
-
-/**
-    void SenseAim() {
-        float lTrig = Input.GetAxis("LeftTrigger");
-        if (lTrig > .2) {
-            if (curGunScale >= maxGunScale && !fireable) {
-                curGunScale = maxGunScale;
-                fireable = true;
-                transform.localScale = new Vector3(transform.localScale.x, curGunScale, transform.localScale.z);
-            } else if(curGunScale < maxGunScale) {
-                curGunScale += scaleSpeed * Time.deltaTime;
-                transform.localScale = new Vector3(transform.localScale.x, curGunScale, transform.localScale.z);
-            }
-        } else if(lTrig < .2){
-            if (fireable) {
-                fireable = false;
-            }
-            if (curGunScale < minGunScale) {
-                curGunScale = minGunScale;
-                transform.localScale = new Vector3(transform.localScale.x, curGunScale, transform.localScale.z);
-            } else if(curGunScale > minGunScale) {
-                curGunScale -= scaleSpeed * Time.deltaTime;
-                transform.localScale = new Vector3(transform.localScale.x, curGunScale, transform.localScale.z);
-            }
-        }
-        if (fireable) {
-            SenseShoot();
-        }
-    }
-    **/
