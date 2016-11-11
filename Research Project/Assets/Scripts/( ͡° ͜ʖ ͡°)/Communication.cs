62c3f47d0314499c9397bae38b3014ffd995de52
Collin Fingar <collinfingar@gmail.com>
2016-11-11 15:57:23 -0500

    And let the races begin

---
+++ Research Project/Assets/Scripts/( ͡° ͜ʖ ͡°)/Communication.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using UnityEngine.VR;
+
+public class Communication : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CmdWhatever ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[Command]
+	public void CmdWhatever(){
+		RpcMeh ();	
+	}
+
+	[ClientRpc]
+	public void RpcMeh(){
+		if (isLocalPlayer && VRSettings.enabled) {
+			print ("( ͡° ͜ʖ ͡°)");
+		}
+	}
+}
