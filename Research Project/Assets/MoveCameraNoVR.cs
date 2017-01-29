3f9dcbac4abd3ee77314b7a4df0831e7b7ad8cee
charder <chardergaming@gmail.com>
2017-01-29 11:32:26 -0500

    Re-purposed a lot of materials and things, playing with Light effects.
    I also created a test scene for myself so I can not be a wet noodle programmer.

---
+++ Research Project/Assets/MoveCameraNoVR.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCameraNoVR : MonoBehaviour {
+	private float newX = 0; private float newY = 0; private float newZ = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newX = 0; newY = 0; newZ = 0;
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			newY = Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			newY = -Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			newX = Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			newX = -Time.deltaTime;
+		}
+		transform.position = new Vector3(transform.position.x + newX, transform.position.y + newY, transform.position.z + newZ);
+	}
+
+
+}
---
+++ Research Project/Assets/RotateObjectNoVR.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectNoVR : MonoBehaviour {
+	private float newX = 0; private float newY = 0; private float newZ = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newX = 0; newY = 0; newZ = 0;
+		if (Input.GetKey (KeyCode.W)) {
+			newX += Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			newX += -Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			newY += Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			newY += -Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.Space)) {
+			transform.rotation = Quaternion.identity;
+		}
+
+		transform.Rotate (Vector3.forward * newY*90);
+		transform.Rotate (Vector3.up * newX*90);
+	}
+}
