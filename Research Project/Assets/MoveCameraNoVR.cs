4325817dd82f8d5bb34d7175fe063fc902ec733a
charder <chardergaming@gmail.com>
2017-01-29 22:47:47 -0500

    Updated NoVR testing and updated prong particles.
    Pop Pop, watching polygons drop. (Almost...)

--- Research Project/Assets/MoveCameraNoVR.cs
+++ Research Project/Assets/MoveCameraNoVR.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class MoveCameraNoVR : MonoBehaviour {
 	private float newX = 0; private float newY = 0; private float newZ = 0;
+	ArrayList newControls;
+	public GunController controlRef;
 	// Use this for initialization
 	void Start () {
-		
+		newControls = new ArrayList ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (controlRef != null) {
+			newControls.Clear ();
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				newControls.Add (1f);
+			} else {
+				newControls.Add (0f);
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha2)) {
+				newControls.Add (1f);
+			} else {
+				newControls.Add (0f);
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha3)) {
+				newControls.Add (1f);
+			} else {
+				newControls.Add (0f);
+			}
+			if (Input.GetKeyDown (KeyCode.Alpha4)) {
+				newControls.Add (1f);
+			} else {
+				newControls.Add (0f);
+			}
+			controlRef.ReceiveInput (newControls);
+		}
 		newX = 0; newY = 0; newZ = 0;
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			newY = Time.deltaTime;
