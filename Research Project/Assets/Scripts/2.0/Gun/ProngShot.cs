cea132fe31bed711c8db125b46c1710c4975f91d
Collin Fingar <collinfingar@gmail.com>
2017-01-30 20:45:45 -0500

    You should have seen by the look in my eyes, baby... there was somethin' missin'.....

--- Research Project/Assets/Scripts/2.0/Gun/ProngShot.cs
+++ Research Project/Assets/Scripts/2.0/Gun/ProngShot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ProngShot : ShotScript {
 
 	public Vector3 velocity = new Vector3 ();
+	public GameObject prongParticle;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,10 @@ public class ProngShot : ShotScript {
 	// Update is called once per frame
 	void Update () {
 		transform.position = transform.position + velocity * Time.deltaTime;
+	}
+
+	void OnDestroy() {
+		GameObject particle = (GameObject)Instantiate (prongParticle, new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z),Quaternion.identity);
+		Destroy (particle, 1.0f); //destroy particlesystem object after 2 seconds.
 	}
 }
