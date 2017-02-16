6447f4d0e367319f16a3ba00620788891b237a99
charder <chardergaming@gmail.com>
2017-02-16 16:13:07 -0500

    Testing to see if things updated properly
    Test Test Test Test Test

---
+++ Research Project/Assets/Scripts/2.0/Enemies/EnemySpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+	CheckpointManager checkRef;
+	public int spawnCP;
+	public float spawnDelay; //delay in seconds until this unit spawns upon reaching the spawning checkpoint
+	bool spawning = false;
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<MeshRenderer> ().enabled = false;
+		checkRef = FindObjectOfType<CheckpointManager> ().GetComponent<CheckpointManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!spawning && checkRef.currentCP >= spawnCP) {
+
+		}
+	}
+
+	void OnDestroy() {
+		checkRef.enemyCount [checkRef.currentCP]--; //subtract this enemy from the enemies remaining
+	}
+}
