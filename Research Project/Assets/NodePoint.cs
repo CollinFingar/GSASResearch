bbadc977f32f6289fb99eef94d1a511b2e81f6d3
charder <chardergaming@gmail.com>
2017-02-16 16:00:51 -0500

    Merge Push
    Let me push again to fix a few things I think. Yeah? Cool.

--- Research Project/Assets/NodePoint.cs
+++ Research Project/Assets/NodePoint.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NodePoint : MonoBehaviour {
+	CheckpointManager checkRef;
 	public int nodeNum; //which number this checkpoint is in the level (used in triggering progression)
 	public int spawnCP; //starting checkpoint when it first spawns
+	public int maxCP; //maximum checkpoint that can be achieved at this node
 	public int endCP; //end checkpoint when it de-spawns 
 	bool active;
 	// Use this for initialization
 	void Start () {
-		
+		checkRef = FindObjectOfType<CheckpointManager> ().GetComponent<CheckpointManager> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!active && checkRef.currentCP >= spawnCP) {
+			activatePoint ();
+		} else if (checkRef.currentCP >= endCP) {
+			deactivatePoint ();
+		}
 	}
-
 	//when the player teleports to this node
 	void activatePoint () {
 		active = true;
